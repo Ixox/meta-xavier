d48949c1ff4260d542a6fd59c314f96eeb982c22
xavier <xavier.hosxe@gmail.com>
2017-03-14 20:45:05 +0100

    Script that update TimoChess on the rpi

---
+++ conf/uploadTimoChess.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cp  ./tmp/work/cortexa7hf-neon-vfpv4-poky-linux-gnueabi/timochess/1.0-r0/deploy-rpms/cortexa7hf_neon_vfpv4/timochess-1.0-r0.cortexa7hf_neon_vfpv4.rpm .
+
+echo ">> Copying timochess package on the raspberry pi 2..."
+export SSHPASS=
+sshpass -e sftp -oBatchMode=no -b - root@192.168.2.21 << !
+   cd /home/root
+   put timochess-1.0-r0.cortexa7hf_neon_vfpv4.rpm
+   bye
+!
+
+rsh -l root "192.168.2.21" << !
+echo ">> Kill TimoChess..."
+killall TimoChess
+echo ">> Installing timochess..."
+cd /home/root
+rpm -Uhv --force  timochess-1.0-r0.cortexa7hf_neon_vfpv4.rpm
+echo ">> Run the NEW TimoChess..."
+TimoChess &
+exit
+!
+
+echo ">> Done"
+
+
+
